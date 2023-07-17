ce13eafa043801c646098c7ffd22d8dd3a6394b9
ramsbottom4 <68180965+ramsbottom4@users.noreply.github.com>
2023-07-17 20:54:59 +0700

    feat: add welcome page view

---
+++ Croid-MRT/Croid-MRT/WelcomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomePageView.swift
+//  Croid-MRT
+//
+//  Created by Michael Christopher on 17/07/23.
+//
+
+import SwiftUI
+
+struct WelcomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomePageView()
+    }
+}
