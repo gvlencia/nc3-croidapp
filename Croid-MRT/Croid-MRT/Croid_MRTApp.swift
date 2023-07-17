9ee2f824bac707ca5e9ac60ae78ae965ba4e4e88
Gaizka Valencia <31756055+gvlencia@users.noreply.github.com>
2023-07-17 15:03:58 +0700

    New Project

---
+++ Croid-MRT/Croid-MRT/Croid_MRTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Croid_MRTApp.swift
+//  Croid-MRT
+//
+//  Created by Gaizka Valencia on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Croid_MRTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
