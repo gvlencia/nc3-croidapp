e2485f90d79e45964fcebcf3874f4c9cad9e5d83
ramsbottom4 <68180965+ramsbottom4@users.noreply.github.com>
2023-07-17 20:57:05 +0700

    feat: add welcome page views

--- Croid-MRT/Croid-MRT/WelcomePageView.swift
+++ Croid-MRT/Croid-MRT/WelcomePageView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct WelcomePageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Group{
+                Text("CRO")
+                    .foregroundColor(Color(red: 0, green: 0.34, blue: 0.72))
+                    .fontWeight(.heavy)
+                    .font(.system(size: 40)) +
+                Text("ID")
+                    .foregroundColor(Color(red: 0.27, green: 0.73, blue: 0.24))
+                    .fontWeight(.heavy)
+                    .font(.system(size: 40))
+            }
+            Text("Ketahui Kepadatan Gerbong MRT Anda")
+                .font(.system(size: 15))
+                .foregroundColor(Color(red: 0.56, green: 0.56, blue: 0.58))
+            Spacer()
+            Image("placeholder")
+            Text("Jadikan #UbahJakarta lebih nyaman dan aman dengan mengetahui tingkat keramaian MRT anda")
+                .font(.system(size:12))
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color(red: 0.39, green: 0.39, blue: 0.4))
+                .frame(width: 285, alignment: .top)
+            Spacer()
+            Spacer()
+            Button {
+                
+            } label: {
+                Label("Cek Keramaian Sekarang", systemImage: "paperplane.fill")
+                    .padding()
+                    .font(.system(size: 17))
+                    .fontWeight(.bold)
+                    .frame(width: 346)
+                    .foregroundColor(.white)
+                    .background(Color(red: 0, green: 0.34, blue: 0.72))
+                    .clipShape(
+                        Capsule()
+                    )
+            }.padding(.bottom, 52)
+            
+        }
     }
 }
 
